3c8163d8f7f3c39c4029c1bc144ec80d7b19c46c
root <root@localhost.localdomain>
2022-05-23 13:50:52 +0800

    First commit 20220523

---
+++ SDL_Game_Demo/demo01.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int agvc, char* agvs[])
+{
+	std::cout << "msn" << std::endl;
+	return 0;
+}
---
+++ test2.txt
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+	std::cout << "msn" << std::endl;
+	return 0;
+}
